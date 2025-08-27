56a2c48b8f4e2996ed236bb428b7f2ba1746e051
Om Roy <omroy2424@gmail.com>
2025-08-27 09:35:55 +0530

    Time: 0 ms (100%), Space: 8.6 MB (49.7%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,19 +1,25 @@
+// class Solution {
+// public:
+//     int GCD(int a, int b){
+//         if(a == 0){
+//             return b;
+//         }
+//         return GCD(b%a,a);
+//     }
+//     int gcdOfOddEvenSums(int n) {
+//         int o =0;
+//         int e = 0;
+//         for(int i = 1;i < n+1 ;i++){
+//             o += (2*i-1);
+//             e += (2*i);
+//         }
+//         cout<<o<<e;
+//         return GCD(o,e);
+//     }
+// };
 class Solution {
 public:
-    int GCD(int a, int b){
-        if(a == 0){
-            return b;
-        }
-        return GCD(b%a,a);
-    }
     int gcdOfOddEvenSums(int n) {
-        int o =0;
-        int e = 0;
-        for(int i = 1;i < n+1 ;i++){
-            o += (2*i-1);
-            e += (2*i);
-        }
-        cout<<o<<e;
-        return GCD(o,e);
+        return n;
     }
 };
