5fe9168c3f68413d7ca0b925d0bb0a84c40f9081
Om Roy <omroy2424@gmail.com>
2025-09-14 16:33:32 +0530

    Added solution - LeetHub

---
+++ 878740-arithmetic-operators-GFG/878740-arithmetic-operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int x , y;
+    cin>>x>>y;
+    cout<<x+y<<" "<<x-y<<" "<<x*y<<" "<<x/y<<" "<<x%y<<endl;
+
+    return 0;
+}
