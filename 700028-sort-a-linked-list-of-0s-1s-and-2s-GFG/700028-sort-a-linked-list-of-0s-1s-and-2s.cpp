43d9672c04a9d0517996006dd52702cd395e9444
Om Roy <omroy2424@gmail.com>
2025-09-18 07:40:51 +0530

    Added solution - LeetHub

---
+++ 700028-sort-a-linked-list-of-0s-1s-and-2s-GFG/700028-sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -0,0 +1,47 @@
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        int z =0 , o = 0, t = 0;
+        Node* temp = head;
+        while(temp){
+            if(temp->data == 0){
+                z++;
+            }
+            else if(temp->data == 1){
+                o++;
+            }
+            else{
+                t++;
+            }
+            temp = temp->next;
+        }
+        temp = head;
+         while (z--) {
+            temp->data = 0;
+            temp = temp->next;
+        }
+        while (o--) {
+            temp->data = 1;
+            temp = temp->next;
+        }
+        while (t--) {
+            temp->data = 2;
+            temp = temp->next;
+        }
+
+        return head;
+    }
+};
