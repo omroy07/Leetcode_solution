c9550b2aaeb5401f1d69825f38a8fdb74b980372
Om Roy <omroy2424@gmail.com>
2025-11-07 22:04:38 +0530

    Time: 34 ms (20.82%), Space: 23.7 MB (94.73%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,23 @@
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+        
+        // Loop through each string in the input vector
+        for (string s : strs) {
+            string sortedStr = s;  // Create a copy of the string to sort
+            sort(sortedStr.begin(), sortedStr.end());  // Sort the copy
+            anagramGroups[sortedStr].push_back(s);  // Group the original string by the sorted version
+        }
+        
+        // Prepare the result vector
+        vector<vector<string>> result;
+        for (auto& group : anagramGroups) {
+            result.push_back(group.second);  // Add each group of anagrams to the result
+        }
+        
+        return result;
+    }
+};
