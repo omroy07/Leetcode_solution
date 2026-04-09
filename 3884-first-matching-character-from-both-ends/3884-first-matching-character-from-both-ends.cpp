ca2151ae82b10aeb752fb753dd3dc4867fb3cb84
Om Roy <omroy2424@gmail.com>
2026-04-09 15:22:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.45%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int firstMatchingIndex(string s) {
-        if(s.size() == 1){
-            return 0;
-        }
         int l = 0 , h = s.length() - 1;
         while(l <= h){
             if(s[l] == s[h]){
