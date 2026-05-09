bb0f70ff06a0948e4e62be853c07eac7dcb98f38
Om Roy <omroy2424@gmail.com>
2026-05-09 13:44:07 +0530

    Time: 0 ms (100%), Space: 9.4 MB (40.93%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.empty()){
+            return res;
+        }
+        unordered_map<char,string> dtl = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+        backtrack(digits,0,"",res,dtl);
+        return res;
+    }
+    void backtrack(const string& digits, int idx, string comb, vector<string>& res, const unordered_map<char, string>& dtl){
+        if(idx == digits.length()){
+            res.push_back(comb);
+            return;
+        }
+        string letters = dtl.at(digits[idx]);
+        for(char l :letters){
+            backtrack(digits,idx+1,comb+l,res,dtl);
+        }
+    }
+};
