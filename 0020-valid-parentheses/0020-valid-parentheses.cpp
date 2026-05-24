0caff97da6a37bcd79dd9f0fcf753df4f58b4ce8
Om Roy <omroy2424@gmail.com>
2026-05-24 09:23:04 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.2%) - LeetHub - Time: O(?), Space: O(?)

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -19,3 +19,8 @@ public:
         return st.empty();
     }
 };
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
