e7c05523bfa1c2fed8b1575ca826073f02ea59d0
Om Roy <omroy2424@gmail.com>
2026-05-27 11:23:44 +0530

    Time: 52 ms (11.59%), Space: 92 MB (57.69%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,31 @@
+#include <stack>
+#include <utility>
+
+class StockSpanner {
+    // The stack will store pairs of {price, span}
+    std::stack<std::pair<int, int>> st;
+
+public:
+    StockSpanner() {
+    }
+    
+    int next(int price) {
+        int span = 1;
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price, span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
