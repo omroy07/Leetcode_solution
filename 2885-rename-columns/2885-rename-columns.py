49d85f316f2cf99f84ec7dde117c8a123c03caea
Om Roy <omroy2424@gmail.com>
2026-06-26 13:59:07 +0530

    Time: 290 ms (33.48%), Space: 66.3 MB (78.5%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2885-rename-columns/2885-rename-columns.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students.rename(columns={'id': 'student_id', 'first': 'first_name', 'last': 'last_name', 'age': 'age_in_years'}, inplace=True)
+    return students
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
