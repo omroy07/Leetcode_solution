4e114ce773b34e706ab6ec8af0400bf5c1fb383b
Om Roy <omroy2424@gmail.com>
2026-06-30 09:43:26 +0530

    Time: 252 ms (67.51%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+UPDATE salary SET sex = 
+CASE sex
+     WHEN 'm' THEN 'f'
+     ELSE 'm'
+END;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
