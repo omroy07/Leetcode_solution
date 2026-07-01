111b16925f55029ac3d0eb7d5cd5d79edf75770a
Om Roy <omroy2424@gmail.com>
2026-07-01 09:34:49 +0530

    Time: 528 ms (55.34%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT stock_name,
+    SUM(CASE WHEN operation = 'Sell' THEN price
+		   ELSE -price END) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
