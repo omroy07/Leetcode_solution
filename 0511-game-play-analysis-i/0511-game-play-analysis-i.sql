347bd2545432444eed87e751111457f34c2ca2d9
Om Roy <omroy2424@gmail.com>
2026-07-06 09:21:49 +0530

    Time: 460 ms (98.83%), Space: 0B (100%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select player_id,min(event_date) as first_login
+from Activity
+group by player_id
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
