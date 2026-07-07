7d510112d501775f66273d94d2817bf11df4d90a
Om Roy <omroy2424@gmail.com>
2026-07-07 11:13:00 +0530

    Time: 479 ms (99.21%), Space: 0B (100%) - LeetHub - Time: O, Space: O(?)

---
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -0,0 +1,21 @@
+SELECT id, 
+    MAX(IF(month="Jan", revenue, NULL)) AS Jan_Revenue, 
+    MAX(IF(month="Feb", revenue, NULL)) AS Feb_Revenue, 
+    MAX(IF(month="Mar", revenue, NULL)) AS Mar_Revenue,
+    MAX(IF(month="Apr", revenue, NULL)) AS Apr_Revenue,
+    MAX(IF(month="May", revenue, NULL)) AS May_Revenue,
+    MAX(IF(month="Jun", revenue, NULL)) AS Jun_Revenue,
+    MAX(IF(month="Jul", revenue, NULL)) AS Jul_Revenue,
+    MAX(IF(month="Aug", revenue, NULL)) AS Aug_Revenue,
+    MAX(IF(month="Sep", revenue, NULL)) AS Sep_Revenue,
+    MAX(IF(month="Oct", revenue, NULL)) AS Oct_Revenue,
+    MAX(IF(month="Nov", revenue, NULL)) AS Nov_Revenue,
+    MAX(IF(month="Dec", revenue, NULL)) AS Dec_Revenue
+    
+FROM Department
+GROUP BY id
+ORDER BY id
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
