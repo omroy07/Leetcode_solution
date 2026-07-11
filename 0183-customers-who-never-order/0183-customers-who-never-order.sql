7c0b18cc361e9f4b13ffbdc7c970ab0ba1d408ac
Om Roy <omroy2424@gmail.com>
2026-07-11 09:52:00 +0530

    Time: 498 ms (99.76%), Space: 0B (100%) - LeetHub - Time: O(n + m), Space: O(n + m)

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT name as Customers
+from Customers
+where id not in (
+    select customerId
+    from Orders
+);
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
