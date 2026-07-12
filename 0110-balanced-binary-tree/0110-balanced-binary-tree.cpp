762fb0df270b157df7bb39d092ef3399f9863ece
Om Roy <omroy2424@gmail.com>
2026-07-12 14:38:17 +0530

    Time: 0 ms (100%), Space: 23 MB (55.76%) - LeetHub - Time: O(n), Space: O(h)

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -33,3 +33,7 @@ private:
             return max(l,r) + 1;
         }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
