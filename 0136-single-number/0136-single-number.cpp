6b72226bb75cea4ad9b183b0d057e757aadcab7f
Om Roy <omroy2424@gmail.com>
2026-09-18 17:02:38 +0530

    Time: 7 ms (18.15%), Space: 20.6 MB (83.8%) - LeetHub - Time: O(n log n), Space: O(log n)

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+                return nums[i-1];
+        }
+        return nums[nums.size()-1];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
